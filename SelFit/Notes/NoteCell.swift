06413c0c347114d888d54f8cb725249aadae1c12
reathanluo <s10205205@connect.np.edu.sg>
2022-02-04 11:49:38 +0800

    update notes

--- SelFit/Notes/NoteCell.swift
+++ SelFit/Notes/NoteCell.swift
@@ -5,9 +5,10 @@
 //  Created by mad2 on 4/2/22.
 //
 
-import Foundation
 import UIKit
 
-class NoteCell: UITableViewCell{
-    
+class NoteCell: UITableViewCell
+{
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
 }
